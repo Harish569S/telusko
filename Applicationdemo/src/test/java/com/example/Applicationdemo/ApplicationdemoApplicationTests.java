4d7fdb08aec36597cf15ce8785ac9110aec534ec
Harish S <harishmys569@gmil.com>
2022-08-10 20:54:36 +0530

    My first move from personal pc

---
+++ Applicationdemo/src/test/java/com/example/Applicationdemo/ApplicationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Applicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
